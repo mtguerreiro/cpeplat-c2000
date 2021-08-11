46a6df43b1a87c3635d0764156132cc5dd6346cb
7LMS7 <86730458+7LMS7@users.noreply.github.com>
2021-08-11 15:10:49 +0200

    Synchronisation Test added

--- cpu2/main.c
+++ cpu2/main.c
@@ -1053,6 +1053,8 @@ static __interrupt void  mainIPC0ISR(void){
 //-----------------------------------------------------------------------------
 static __interrupt void mainADCAISR(void){
 
+    GPIO_togglePin(PLAT_CPU2_GPIO_2);                       //Synchronisation Test
+
     float u;
 
     AdcaRegs.ADCINTFLGCLR.bit.ADCINT1 = 1;      // Clear ADC INT1 flag
